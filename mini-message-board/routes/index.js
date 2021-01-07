755b45bb87bb16eb932e5c33ee0e63575cab5537
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-01-06 19:45:35 -0800

    Submit adds new message and redirects to messages page

--- mini-message-board/routes/index.js
+++ mini-message-board/routes/index.js
@@ -18,4 +18,21 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Mini Message Board', messages });
 });
 
+/* NEW message. */
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: 'Add New Message' });
+});
+
+router.post('/new', function (req, res) {
+  console.log(req.body.name);
+  console.log(req.body.message);
+  const newMessage = {
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date()
+  };
+  messages.push(newMessage);
+  res.redirect('/');
+});
+
 module.exports = router;
